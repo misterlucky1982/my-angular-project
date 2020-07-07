a4a0b9ed5419b392d918fd0ae19d3a01fb5b0302
dubovika <Andrei.Dubovik@standardbank.co.za>
2020-07-07 14:14:54 +0300

    ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('apples',5),
+    new Ingredient('potato', 8),
+    new Ingredient('tomatoes', 11)
+  ];
 
   constructor() { }
 
