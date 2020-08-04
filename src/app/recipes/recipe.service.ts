22c3492ba3f6ead30cf4ff68fe4cfd793bf267b2
dubovika <Andrei.Dubovik@standardbank.co.za>
2020-08-04 14:27:30 +0300

    add to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService{
   private recipes: Recipe[] = [
     new Recipe('Recipe A',
@@ -20,7 +22,14 @@ export class RecipeService{
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes(){
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingListService.addIngredientsToList(ingredients);
   }
 }
